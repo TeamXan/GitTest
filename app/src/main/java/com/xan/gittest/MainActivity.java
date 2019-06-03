d73b87e782c34b083e8dfa579ef146fc13161479
yma <yemyintaung.aung@gmail.com>
2019-06-03 16:19:00 +0630

    Ei Ei

--- app/src/main/java/com/xan/gittest/MainActivity.java
+++ app/src/main/java/com/xan/gittest/MainActivity.java
@@ -20,4 +20,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
